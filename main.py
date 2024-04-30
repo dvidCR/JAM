622afed3189df86df04dca4b0603fe24b0335474
dav200435 <davacas2005@gmail.com>
2024-04-30 17:51:24 +0200

    camara que sigue al jugador

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
+
+player_x = 400
+player_y = 300
+player_speed = 5
+
+# Definir la posición de la cámara
+camera_x = 0
+camera_y = 0
+
+
 
 while running:
     # poll for events
@@ -11,11 +24,27 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    camera_x = player_x - SCREEN_WIDTH / 2
+    camera_y = player_y - SCREEN_HEIGHT / 2
+
+    keys = pygame.key.get_pressed()
+
+    # Mover el personaje
+    if keys[pygame.K_LEFT]:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player_x += player_speed
+    if keys[pygame.K_UP]:
+        player_y -= player_speed
+    if keys[pygame.K_DOWN]:
+        player_y += player_speed
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, (255, 0, 0), (int(player_x - camera_x), int(player_y - camera_y)), 20)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
