920120180790bd9ff2cd5dc31b5fb1a42cb79885
Alfonso <adeuna@colegiolagomar.es>
2024-04-30 19:34:19 +0200

    create platform

---
+++ platformt.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+import sys 
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, xloc, yloc, imgw, imgh, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img)).convert()
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.y = yloc
+        self.rect.x = xloc
+        
+        
+        
